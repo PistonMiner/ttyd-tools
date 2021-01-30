21281efb8d7941569aa9a8b55a9212455a55abd5
PistonMiner <pistonminer@gmail.com>
2021-01-30 02:23:57 +0100

    rel: Add header and symbols for OSContext

--- ttyd-tools/rel/include/gc/os.h
+++ ttyd-tools/rel/include/gc/os.h
@@ -43,6 +43,51 @@ void DCStoreRange(void *base, uint32_t size);
 void DCFlushRangeNoSync(void *base, uint32_t size);
 void DCStoreRangeNoSync(void *base, uint32_t size);
 
+}
+
+// OSContext
+struct OSContext
+{
+	uint32_t gpr[32];
+	uint32_t cr;
+	uint32_t lr;
+	uint32_t ctr;
+	uint32_t xer;
+	double fpr[32];
+	double fpcsr;
+	uint32_t srr0;
+	uint32_t srr1;
+
+	uint16_t mode;
+	uint16_t state;
+
+	uint32_t gqr[8];
+	
+	uint32_t pad_1c4;
+
+	double fpr_ps[32];
+} __attribute__((__packed__));
+
+extern "C" {
+
+void OSInitContext(OSContext *context, uint32_t srr0, uint32_t r1);
+void OSClearContext(OSContext *context);
+
+int32_t OSSaveContext(OSContext *context);
+void OSLoadContext(OSContext *context);
+
+OSContext *OSGetCurrentContext();
+void OSSetCurrentContext(OSContext *context);
+
+void OSSaveFPUContext(OSContext *context);
+void OSFillFPUContext(OSContext *context);
+void __OSSaveFPUContext(uint32_t, uint32_t, OSContext *context);
+void __OSLoadFPUContext(uint32_t, OSContext *context);
+
+void OSDumpContext(OSContext *context);
+
+void *OSGetStackPointer(OSContext *context);
+
 // OSError
 void OSReport(const char *fmt, ...);
 void OSPanic(const char *fmt, ...);

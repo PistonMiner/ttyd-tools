4324eec89ef088467ea90b9339a8b3c0177ef943
Seeky <seeky.mkw@gmail.com>
2021-06-22 17:08:07 +0100

    elf2rel: Add support for linking against other rels
    Symbols for other rel modules can now be added to lst files with the syntax addr:symbolName?moduleId,sectionId

--- ttyd-tools/elf2rel/elf2rel.cpp
+++ ttyd-tools/elf2rel/elf2rel.cpp
@@ -13,9 +13,16 @@
 #include <tuple>
 #include <deque>
 
-std::map<std::string, uint32_t> loadSymbolMap(const std::string &filename)
+struct SymbolLocation
 {
-	std::map<std::string, uint32_t> outputMap;
+	uint32_t moduleId; // 0 means dol
+	uint32_t targetSection; // OSLink ignores for dol
+	uint32_t addr;
+};
+
+std::map<std::string, SymbolLocation> loadSymbolMap(const std::string &filename)
+{
+	std::map<std::string, SymbolLocation> outputMap;
 
 	std::ifstream inputStream(filename);
 	for (std::string line; std::getline(inputStream, line); )
@@ -28,14 +35,35 @@ std::map<std::string, uint32_t> loadSymbolMap(const std::string &filename)
 			continue;
 		}
 
-		size_t index = line.find_first_of(':');
+		// dol symbols:    addr:symbolName
+		// rel symbols:    addr:symbolName?moduleId,sectionId
+		size_t index = line.find_first_of(':'); // addr-name separator
+		size_t index2 = line.find_first_of('?', index); // name-module separator
+		size_t index3 = line.find_first_of(',', index2); // module-section separator
+		bool dol = (index2 == std::string::npos) || (index3 == std::string::npos);
 
-		std::string name = line.substr(index + 1);
+		std::string name;
+		if (dol)
+			name = line.substr(index + 1);
+		else
+			name = line.substr(index + 1, index2 - (index + 1));
 		boost::trim_left(name);
 
 		uint32_t addr = strtoul(line.substr(0, index).c_str(), nullptr, 16);
 
-		outputMap[name] = addr;
+		uint32_t moduleId;
+		if (dol)
+			moduleId = 0;
+		else
+			moduleId = strtoul(line.substr(index2 + 1, index3).c_str(), nullptr, 10);
+
+		uint32_t sectionId;
+		if (dol)
+			sectionId = 0;
+		else
+			sectionId = strtoul(line.substr(index3 + 1).c_str(), nullptr, 10);
+
+		outputMap[name] = { moduleId, sectionId, addr };
 	}
 
 	return outputMap;
@@ -382,10 +410,9 @@ int main(int argc, char **argv)
 					{
 						// Known external!
 						resolved = true;
-
-						rel.moduleID = 0;
-						rel.targetSection = 0; // #todo-elf2rel: Check if this is important
-						rel.addend = static_cast<uint32_t>(addend + it->second);
+						rel.moduleID = it->second.moduleId;
+						rel.targetSection = it->second.targetSection;
+						rel.addend = static_cast<uint32_t>(addend + it->second.addr);
 					}
 				}
 
